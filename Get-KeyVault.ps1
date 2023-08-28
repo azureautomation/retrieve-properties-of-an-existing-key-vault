86b99017f4563a5c6ac28670d3cbbd3be6b5c77e
Nina <47805999+MSNina123456@users.noreply.github.com>
2023-08-28 12:24:09 +0800

    Update Get-KeyVault.ps1

--- Get-KeyVault.ps1
+++ Get-KeyVault.ps1
@@ -6,51 +6,46 @@
   This runbook will retrieve a key vault if one exists.
 
   PREREQUISITE
-  The automation account has to be created with a "Run As" account (Service Principal). Automation accounts with a run as account 
-  can be identified by verifying that they contain the two assets: "AzureRunAsConnection" and "AzureRunAsCertificate". 
-  See https://azure.microsoft.com/en-us/documentation/articles/automation-sec-configure-azure-runas-account/ for more information.
+  This runbook uses system maanged identity for Azurelogin. Assign appropriate role for system managed identity under this Automation account before start this runbook.
+
+  More details: https://learn.microsoft.com/azure/automation/enable-managed-identity-for-automation#assign-role-to-a-system-assigned-managed-identity
 
   PREREQUISITE
-  The modules "AzureRM.profile" and "AzureRM.keyvault" have to be imported into the automation account. They can be imported from
-  the Module Gallery. See https://azure.microsoft.com/en-us/blog/announcing-azure-resource-manager-support-azure-automation-runbooks/
-  for more information.
+  The modules "Az.Accounts" and "Az.KeyVault" have to be imported into the automation account. They can be imported from
+  the Module Gallery. 
 
 .PARAMETER VaultName 
   Mandatory
   This is the name of the key vault.
   
-.PARAMETER ResourceGroupName 
-  Mandatory
-  This is the name of the resource group that the key vault is in.
-  
 .NOTES
-  AUTHOR: Andreas Wieberneit
-  LASTEDIT: April 15, 2016
+  AUTHOR: Nina Li
+  LASTEDIT: June 20, 2023
 #>
 
 param
 (
 	[Parameter (Mandatory=$true)]
 	[string] $VaultName,
-	[Parameter (Mandatory=$true)]
-	[string] $ResourceGroupName
+    [Parameter (Mandatory=$true)]
+	[string] $SubscriptionId
 )
 
-# Authenticate to Azure with service principal and certificate, and set subscription
-$connectionAssetName = "AzureRunAsConnection"
-$conn = Get-AutomationConnection -Name $ConnectionAssetName
-if ($conn -eq $null)
-{
-	throw "Could not retrieve $connectionAssetName connection asset. Check that this asset exists in the automation account."
-}
-Add-AzureRmAccount -ServicePrincipal -Tenant $conn.TenantID -ApplicationId $conn.ApplicationId -CertificateThumbprint $conn.CertificateThumbprint -ErrorAction Stop | Write-Verbose
-Set-AzureRmContext -SubscriptionId $conn.SubscriptionId -ErrorAction Stop | Write-Verbose
+"Logging in to Azure..."
+    # Ensures you do not inherit an AzContext in your runbook
+    Disable-AzContextAutosave -Scope Process
+
+    # Connect to Azure with system-assigned managed identity
+    Connect-AzAccount -Identity
+
+    # set and store context
+    $AzureContext = Set-AzContext -SubscriptionId $SubscriptionId
 
 # Try to retrieve the key vault.
-$keyVault = Get-AzureRMKeyVault -VaultName $VaultName -ResourceGroupName $ResourceGroupName 
+$keyVault = Get-AzKeyVault -VaultName $VaultName
 if ($keyVault -eq $null)
 {
-	throw "Could not retrieve key vault $VaultName. Check that a key vault with this name exists in the resource group $ResourceGroupName."
+	throw "Could not retrieve key vault $VaultName. Check that a key vault with this name exists."
 }
 
 # Return the key vault
